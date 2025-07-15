073b7f3fc14deb28b416a35e726ee15512e4cbc3
Aykut Güven <naykutguven@gmail.com>
2025-07-15 18:56:06 +0200

    Implement morphing message menu effect

---
+++ CoolSwiftUIAnimations/iMessage Morph Menu Effect/MessageMenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageMenuView.swift
+//  CoolSwiftUIAnimations
+//
+//  Created by Aykut Güven on 15.07.25.
+//
+
+import SwiftUI
+
+struct MessageMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageMenuView()
+}
